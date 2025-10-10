049c0118c1e474d6b50cc443a75ae9de3ff7ab9a
Arlebon <arnaudlebonseven@gmail.com>
2025-10-10 14:34:08 +0200

    Add project files.

---
+++ EntityToolBox/BaseRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityToolBox
+{
+    public class BaseRepository<T> where T : class
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _set;
+
+        public BaseRepository(DbContext context)
+        {
+            _context = context;
+            _set = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _set;
+        }
+
+        public T? GetOne(int id)
+        {
+            return _set.Find(id);
+        }
+
+        public void Add(T entity)
+        {
+            _set.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Add(IEnumerable<T> entities)
+        {
+            _set.AddRange(entities);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _set.Update(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _set.Remove(entity);
+            _context.SaveChanges();
+        }
+    }
+}
