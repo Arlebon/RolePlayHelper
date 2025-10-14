abd7b99ccf233c455eda37b07ca9b9d40aa54b73
Raphael <rp.capitam@outlook.de>
2025-10-14 23:33:36 +0200

    race

---
+++ RolePlayHelper.DL/Entities/Race.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace RolePlayHelper.DL.Entities
+{
+    public class Race
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public IEnumerable<RaceTrait> traits { get; set; } = [];
+
+        public IEnumerable<Language> languages { get; set; } = [];
+
+
+    }
+}
---
+++ RolePlayHelper.DL/Entities/RaceTrait.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RolePlayHelper.DL.Entities
+{
+    internal class RaceTrait
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public Dictionary<int StatModifyer, int Value> StatModifiers { get; set; }
+
+    }
+}
