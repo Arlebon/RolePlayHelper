aaddfde1140b22b981b3a26b3141598d84c45545
raphael <rp.capital@outlook.de>
2025-10-16 17:00:00 +0200

    character Class init

---
+++ RolePlayHelper.DL/Entities/CharClass.cs
@@ -0,0 +1,18 @@
+﻿namespace RolePlayHelper.DL.Entities
+{
+    public class CharClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+
+
+        // A Character (Sub)Class can have a parentClass
+        public int? ParentCharacterId { get; set; }
+        public CharClass? ParentClass { get; set; }
+
+        // A ParentClass can have severall Subclasses
+        public List<CharClass>? SubClasses { get; set; } = new();
+
+    }
+}
