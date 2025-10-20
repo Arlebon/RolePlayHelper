1293b78683282837af4f3565c8d24826730edf95
Arlebon <arnaudlebonseven@gmail.com>
2025-10-20 15:43:50 +0200

    Restore character models

---
+++ RolePlayHelper.API/Mappers/CharacterMapper.cs
@@ -0,0 +1,33 @@
+﻿using RolePlayHelper.API.Models.Character;
+using RolePlayHelper.DL.Entities;
+
+namespace RolePlayHelper.API.Mappers
+{
+    public static class CharacterMapper
+    {
+        public static CharacterListDto ToCharacterListDto(this Character character)
+        {
+            return new CharacterListDto()
+            {
+                Id = character.Id,
+                Name = character.Name,
+                RaceName = character.Race.Name,
+            };
+        }
+
+        public static Character ToCharacter(this CharacterFormDto form)
+        {
+            return new Character()
+            {
+                Name = form.Name,
+                STR = form.STR,
+                DEX = form.DEX,
+                CHA = form.CHA,
+                INT = form.INT,
+                CON = form.CON,
+                WIS = form.WIS,
+                RaceId = form.RaceId,
+            };
+        }
+    }
+}
